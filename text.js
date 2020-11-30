27a4e7f0110621f1db6945f95691c6e02847af84
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-11-30 13:45:09 +0200

    TextStyle Completed

---
+++ text.js
@@ -0,0 +1,45 @@
+function ChangeFont(fnt)
+{
+    document.getElementById("PAR").style.fontFamily = fnt;
+}
+
+function ChangeAlign(algn)
+{
+    document.getElementById("PAR").style.textAlign = algn;
+}
+
+function ChangeHeight(height)
+{
+    document.getElementById("PAR").style.lineHeight = height;
+}
+
+function ChangeLSpace(spc)
+{
+    document.getElementById("PAR").style.letterSpacing = spc;
+}
+
+function ChangeIndent(ind)
+{
+    document.getElementById("PAR").style.textIndent= ind;
+}
+
+function ChangeTransform(trn)
+{
+    document.getElementById("PAR").style.textTransform= trn;
+}
+
+function ChangeDecorate(dec)
+{
+    document.getElementById("PAR").style.textDecoration = dec;
+}
+
+function ChangeBorder(brd)
+{
+    document.getElementById("PAR").style.border= brd;
+}
+
+function ChangeBorderColor(color)
+{
+    document.getElementById("PAR").style.borderColor = color;
+}
+
